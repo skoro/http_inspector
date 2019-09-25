dcff8719c14eb7b973534793be1be8115bed0228
Alexei Skorobogatko <skorobogatko.oleksii@gmail.com>
2019-09-25 22:43:23 +0300

    Force to synchronize query params (fixes when the bookmark is moved or copied).

--- source/main.pas
+++ source/main.pas
@@ -663,6 +663,8 @@ begin
     // Set response content.
     if Assigned(ResponseInfo) then
       OnRequestComplete(ResponseInfo);
+
+    SyncGridQueryParams;
   end;
 end;
 
@@ -2011,6 +2013,7 @@ begin
           // Update url for the current bookmark.
           if (FBookManager.CurrentBookmark = BM) and (RO.Url <> Bookmark.Request.Url) then
             cbUrl.Text := Bookmark.Request.Url;
+          SyncGridQueryParams;
         end;
         else begin
           FreeAndNil(RO);
